9c939adda2b3ef63c967fdf694d2a1b2118aecab
few <taramart.k@mutsvmail.com>
2022-02-25 15:40:03 +0700

    7

--- Lab7/ex.if_else.py
+++ Lab7/ex.if_else.py
@@ -3,29 +3,31 @@
 username = 'admin'
 password = '1234'
 
-u = input('Enter username: ')
-p = input('Enter password: ')
+# u = input('Enter username: ')
+# p = input('Enter password: ')
 
 # Login Example 1
 # if u == username and p == password:
-#     print(f'welcome {u}')
+#     print(f'Welcome {u}')
 # else:
 #     print('your username or password is not correct.')
 
-# # Login Example 2
+# Login Example 2
 # if u == username:
 #     if p == password:
-#         print(f'welcome {u}')
+#         print(f'Welcome {u}')
 #     else:
 #         print(f'your password is not correct.')
 # else:
 #     print(f'username is not correct.')
 
 # Login Example 3
-u == input('Enter username: ')
+u = input('Enter username: ')
 if u == username:
-    p == input('Enter password: ')
+    p = input('Enter password: ')
     if p == password:
-        print(f'welcome {u}')
+        print(f'Welcome {u}')
     else:
-        print(f'your pass')
+        print(f'your password is not correct.')
+else:
+     print(f'username is not correct.')
