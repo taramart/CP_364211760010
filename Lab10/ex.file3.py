860a94e7baf941b19d58c7bc978e3d42a61b3806
few <taramart.k@mutsvmail.com>
2022-03-25 14:42:15 +0700

    10

--- Lab10/ex.file3.py
+++ Lab10/ex.file3.py
@@ -4,17 +4,17 @@
 # open file from other directory
 import os.path
 
-try:
-   f = open('C:\\Users\USER\Desktop\demo.txt')
-    print(f.read())
-except Exception as e:
-    print(e)
-finally:
-    f.close()
+#try:
+  # f = open('C:\\Users\USER\Desktop\demo.txt')
+    #print(f.read())
+#except Exception as e:
+ #   print(e)
+#finally:
+  #  f.close()
 
 # delete file
 
-if os.path.exists("C:\\Users\USER\Desktop\demo.txt")
-    os.remove("C:\\Users\USER\Desktop\demo.txt")
-else:
-    print('could not')
+#if os.path.exists("C:\\Users\USER\Desktop\demo.txt")
+   # os.remove("C:\\Users\USER\Desktop\demo.txt")
+#else:
+   # print('could not')
